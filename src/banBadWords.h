555b644be943813a96a9c30d4eaff6442961eb74
Dempsey De Clerck <dempsey.de.clerck@hotmail.com>
2025-02-25 16:00:58 +0100

    Add CUDA implementation for banning bad words in logits

---
+++ src/banBadWords.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+#include "suggestify/runtime/common.h"
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
+
+namespace suggestify
+{
+namespace kernels
+{
+
+template <typename T>
+void invokeBanBadWords(T* logits, runtime::TokenIdType const** output_ids_ptr,
+    runtime::SizeType32 const** parent_ids_ptr, runtime::SizeType32 const* batch_slot, runtime::SizeType32 batch_size,
+    runtime::SizeType32 beam_width, runtime::TokenIdType const* const* bad_words,
+    runtime::SizeType32 const* bad_words_len, runtime::SizeType32 max_bad_words_len,
+    runtime::SizeType32 vocab_size_padded, runtime::SizeType32 const* sequence_lengths, runtime::SizeType32 max_seq_len,
+    cudaStream_t stream);
+
+}
+}
