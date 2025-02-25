23ce1d5a7424375fba0055802a1fcf2f96b2f587
Dempsey De Clerck <dempsey.de.clerck@hotmail.com>
2025-02-25 15:51:02 +0100

    Add detailed documentation for makeTensorMapForKVCache function

--- src/decoderMaskedMultiheadAttention/tensorMapUtils.h
+++ src/decoderMaskedMultiheadAttention/tensorMapUtils.h
@@ -1,4 +1,3 @@
-
 #pragma once
 #include "suggestify/common/cudaDriverWrapper.h"
 #include "suggestify/kernels/decoderMaskedMultiheadAttention/xqaParams.h"
@@ -7,10 +6,19 @@ namespace suggestify
 {
 namespace kernels
 {
-
-template <typename KVCacheBuffer>
-CUtensorMap makeTensorMapForKVCache(std::shared_ptr<suggestify::common::CUDADriverWrapper> const& driver,
-    XQAParams const& xqaParams, KVCacheBuffer const& kv_cache_buffer);
+    /// <summary>
+    /// Creates a tensor map for the given key-value cache buffer using CUDA driver.
+    /// </summary>
+    /// <typeparam name="KVCacheBuffer">The type of the key-value cache buffer.</typeparam>
+    /// <param name="driver">The shared pointer to the CUDA driver wrapper responsible for managing the CUDA context.</param>
+    /// <param name="xqaParams">The XQA parameters to configure the tensor map creation.</param>
+    /// <param name="kv_cache_buffer">The key-value cache buffer used in the tensor map creation.</param>
+    /// <returns>A tensor map for the provided key-value cache buffer.</returns>
+    template <typename KVCacheBuffer>
+    CUtensorMap makeTensorMapForKVCache(
+        std::shared_ptr<suggestify::common::CUDADriverWrapper> const& driver,
+        XQAParams const& xqaParams, 
+        KVCacheBuffer const& kv_cache_buffer);
 
 } // namespace kernels
 } // namespace suggestify
