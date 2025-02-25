3a5b74c1ce3cd6d43e4017404f7d0f66d5a8c52e
Dempsey De Clerck <dempsey.de.clerck@hotmail.com>
2025-02-25 16:28:59 +0100

    Add CUDA kernel implementations for Top-P and Top-K sampling with parameter validation

---
+++ src/samplingTopKKernels.h
@@ -0,0 +1,213 @@
+#pragma once
+
+#include "suggestify/common/memoryUtils.h"
+#include "suggestify/kernels/decodingCommon.h"
+#include "suggestify/runtime/common.h"
+#include <curand_kernel.h>
+
+namespace suggestify::kernels
+{
+
+static constexpr runtime::SizeType32 TOP_K_MAX = 1024;
+
+template <typename T>
+struct TopKSamplingKernelParams
+{
+
+    T const* logProbs{nullptr};
+    T const* const* logProbsPtrs{nullptr};
+
+    runtime::TokenIdType** outputIdsPtrs{nullptr};
+    runtime::TokenIdType* outputIds{nullptr};
+
+    void* workspace{nullptr};
+
+    runtime::TokenIdType const* endIds{nullptr};
+
+    runtime::SizeType32* sequenceLengths{nullptr};
+    runtime::SizeType32 const* batchSlots{nullptr};
+    runtime::SizeType32 const* tokensPerStep{nullptr};
+
+    FinishedState const* finishedInput{nullptr};
+    FinishedState* finishedOutput{nullptr};
+    bool const* skipDecode{nullptr};
+
+    float* cumLogProbs{nullptr};
+    float* outputLogProbs{nullptr};
+
+    curandState_t* curandState{nullptr};
+    runtime::SizeType32 const* topKs{nullptr};
+    float const* topPs{nullptr};
+    runtime::SizeType32 maxTopK{TOP_K_MAX};
+    float maxTopP{1.0f};
+
+    runtime::SizeType32 batchSize{-1};
+    runtime::SizeType32 maxBatchSize{-1};
+    runtime::SizeType32 vocabSizePadded{-1};
+    runtime::SizeType32 maxTokensPerStep{-1};
+    runtime::SizeType32 maxSeqLen{-1};
+
+    bool normalizeLogProbs{false};
+    bool logitsHasProbs{false};
+    bool returnAllSelectedTokens{false};
+
+    runtime::TokenIdType* outputIdCurrentStep{nullptr};
+    bool const* skipOutputIdCurrentStep{nullptr};
+
+    void checkParams() const
+    {
+        TLLM_CHECK(batchSize > 0);
+        TLLM_CHECK(maxBatchSize > 0);
+        TLLM_CHECK(maxBatchSize >= batchSize);
+        TLLM_CHECK(vocabSizePadded > 0);
+        TLLM_CHECK(maxTokensPerStep > 0);
+
+        TLLM_CHECK(logProbs || logProbsPtrs);
+        TLLM_CHECK(outputIds || outputIdsPtrs);
+
+        if (maxTokensPerStep > 1)
+        {
+            TLLM_CHECK(tokensPerStep);
+        }
+
+        if (outputIds)
+        {
+            TLLM_CHECK(maxSeqLen > 0);
+        }
+
+        TLLM_CHECK(workspace);
+
+        TLLM_CHECK(maxTokensPerStep != 1 || returnAllSelectedTokens || sequenceLengths);
+        TLLM_CHECK(maxTokensPerStep != 1 || returnAllSelectedTokens || endIds);
+        if (cumLogProbs != nullptr || outputLogProbs != nullptr)
+        {
+            TLLM_CHECK(maxTokensPerStep == 1);
+            if (cumLogProbs != nullptr)
+            {
+                TLLM_CHECK(!returnAllSelectedTokens);
+            }
+        }
+
+        TLLM_CHECK(((finishedOutput == nullptr) ^ (endIds == nullptr)) == 0);
+
+        TLLM_CHECK(0 < maxTopP && maxTopP <= 1.f);
+        TLLM_CHECK(0 <= maxTopK && maxTopK <= TOP_K_MAX);
+        TLLM_CHECK((skipOutputIdCurrentStep && outputIdCurrentStep && returnAllSelectedTokens)
+            || (skipOutputIdCurrentStep == nullptr && outputIdCurrentStep == nullptr));
+    }
+};
+
+template <typename T>
+void invokeBatchTopKSampling(TopKSamplingKernelParams<T> const& params, cudaStream_t stream);
+
+template <typename T>
+[[nodiscard]] std::vector<size_t> getTopKWorkspaceSizes(runtime::SizeType32 batchSize,
+    runtime::SizeType32 maxTokensPerStep, runtime::SizeType32 maxTopK, runtime::SizeType32 vocabSizePadded)
+{
+    runtime::SizeType32 constexpr maxBlockPerBeam = 8;
+    auto const tempLogProbsBufSize = sizeof(T) * batchSize * maxTokensPerStep * vocabSizePadded;
+    auto const topKTmpIdsBufSize
+        = sizeof(runtime::SizeType32) * batchSize * maxTokensPerStep * maxTopK * maxBlockPerBeam;
+    auto const topKTmpValBufSize = sizeof(T) * batchSize * maxTokensPerStep * maxTopK * maxBlockPerBeam;
+
+    return {tempLogProbsBufSize, topKTmpIdsBufSize, topKTmpValBufSize};
+}
+
+[[nodiscard]] inline std::vector<size_t> getTopKInitWorkspaceSizes(runtime::SizeType32 batchSize)
+{
+    auto const tempTopKsBufSize = batchSize * sizeof(runtime::SizeType32);
+    auto const tempTopPsBufSize = batchSize * sizeof(float);
+
+    return {tempTopKsBufSize, tempTopPsBufSize};
+}
+
+template <typename T>
+[[nodiscard]] size_t getTopKWorkspaceSize(runtime::SizeType32 batchSize, runtime::SizeType32 maxTokensPerStep,
+    runtime::SizeType32 maxTopK, runtime::SizeType32 vocabSizePadded)
+{
+    auto const workspaceSizes = getTopKWorkspaceSizes<T>(batchSize, maxTokensPerStep, maxTopK, vocabSizePadded);
+    auto const initWorkspaceSizes = getTopKInitWorkspaceSizes(batchSize);
+    return std::max(suggestify::common::calcAlignedSize(workspaceSizes, 256),
+        suggestify::common::calcAlignedSize(initWorkspaceSizes, 256));
+}
+
+void invokeSetupTopKRuntimeArgs(runtime::SizeType32 batchSize, ScatterDecodingParamEntry<runtime::SizeType32> topK,
+    ScatterDecodingParamEntry<float> topP, bool* skipDecodePtr, runtime::SizeType32 const* batchSlotsPtr, bool onDevice,
+    cudaStream_t stream = nullptr);
+
+void invokeSetupTopKTopPRuntimeArgs(runtime::SizeType32 batchSize, ScatterDecodingParamEntry<runtime::SizeType32> topK,
+    ScatterDecodingParamEntry<float> topP, bool* skipDecodeTopKPtr, bool* skipDecodeTopPPtr,
+    runtime::SizeType32 const* batchSlotsPtr, bool onDevice, cudaStream_t stream = nullptr);
+
+inline bool clampTopP(float& topP)
+{
+    if (topP < 0.f || topP > 1.0f)
+    {
+        TLLM_LOG_WARNING("TopP (%f) is out of range ([0.0, 1.0f]). Clip to closest number.", topP);
+        topP = std::clamp(topP, 0.f, 1.f);
+        return true;
+    }
+
+    return false;
+}
+
+inline bool clampTopK(runtime::SizeType32& topK)
+{
+    if (topK < 0 || topK > TOP_K_MAX)
+    {
+        TLLM_LOG_WARNING(
+            "TopK (%d) is larger than max supported number (%d). Clip to max supported number.", topK, TOP_K_MAX);
+        topK = std::clamp(topK, 0, TOP_K_MAX);
+        return true;
+    }
+
+    return false;
+}
+
+inline bool regularizeTopKTopP(runtime::SizeType32& topK, float& topP)
+{
+    bool modified = false;
+    if (topK == 0 && topP == 0.0f)
+    {
+        topK = 1;
+        modified = true;
+    }
+    if (topK > 0 && topP == 0.0f)
+    {
+        topP = 1.0f;
+        modified = true;
+    }
+
+    return modified;
+}
+
+__device__ __host__ inline void setupTopKTopPRuntimeArgOne(runtime::SizeType32 batchIndex,
+    ScatterDecodingParamEntry<runtime::SizeType32> topK, ScatterDecodingParamEntry<float> topP,
+    runtime::SizeType32 const* batchSlots, bool* skipDecodeTopK, bool* skipDecodeTopP, float* initialTopPBuf)
+{
+    auto const batchSlot = batchSlots[batchIndex];
+    auto const k = topK.mVector == nullptr ? topK.mScalar : topK.mVector[batchIndex];
+    auto const p = topP.mVector == nullptr ? topP.mScalar : topP.mVector[batchIndex];
+    if (topK.mTarget != nullptr)
+    {
+        topK.mTarget[batchSlot] = k;
+    }
+    if (topP.mTarget != nullptr)
+    {
+        topP.mTarget[batchSlot] = p;
+    }
+    if (skipDecodeTopK != nullptr)
+    {
+        skipDecodeTopK[batchSlot] = k == 0;
+    }
+    if (skipDecodeTopP != nullptr)
+    {
+        skipDecodeTopP[batchSlot] = k != 0;
+    }
+    if (initialTopPBuf != nullptr)
+    {
+        initialTopPBuf[batchSlot] = p;
+    }
+}
+
+}
---
+++ src/samplingTopPKernels.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "suggestify/common/assert.h"
+#include "suggestify/kernels/decodingCommon.h"
+#include "suggestify/runtime/common.h"
+#include <curand_kernel.h>
+
+namespace suggestify::kernels
+{
+template <typename T>
+struct TopPSamplingKernelParams
+{
+    T const* probs{nullptr};
+
+    runtime::TokenIdType** outputIdsPtrs{nullptr};
+
+    runtime::TokenIdType* outputIds{nullptr};
+
+    void* workspace{nullptr};
+
+    float const* topPs{nullptr};
+
+    runtime::SizeType32* sequenceLength{nullptr};
+    runtime::TokenIdType const* endIds{nullptr};
+    runtime::SizeType32 const* batchSlots{nullptr};
+
+    FinishedState const* finishedInput{nullptr};
+    FinishedState* finishedOutput{nullptr};
+    bool const* skipDecode{nullptr};
+
+    float* cumLogProbs{nullptr};
+    float* outputLogProbs{nullptr};
+    curandState_t* curandState{nullptr};
+    float const* randomVals{nullptr};
+
+    runtime::SizeType32 blockNum{-1};
+    bool isDeterministic{true};
+
+    runtime::SizeType32 batchSize{-1};
+    runtime::SizeType32 maxBatchSize{-1};
+    runtime::SizeType32 vocabSizePadded{-1};
+    runtime::SizeType32 maxSeqLen{-1};
+
+    bool returnAllSelectedTokens{false};
+
+    runtime::TokenIdType* outputIdCurrentStep{nullptr};
+    bool const* skipOutputIdCurrentStep{nullptr};
+
+    void checkParams() const
+    {
+        TLLM_CHECK(batchSize > 0);
+        TLLM_CHECK(maxBatchSize > 0);
+        TLLM_CHECK(maxBatchSize >= batchSize);
+        TLLM_CHECK(vocabSizePadded > 0);
+        TLLM_CHECK(probs);
+        TLLM_CHECK(outputIds || outputIdsPtrs);
+        TLLM_CHECK(workspace);
+        TLLM_CHECK((curandState != nullptr) || (randomVals != nullptr));
+        TLLM_CHECK(((curandState != nullptr) & (randomVals != nullptr)) == 0);
+        TLLM_CHECK(topPs);
+
+        if (outputIds)
+        {
+            TLLM_CHECK(maxSeqLen > 0);
+        }
+
+        TLLM_CHECK(((finishedOutput == nullptr) ^ (endIds == nullptr)) == 0);
+        TLLM_CHECK((skipOutputIdCurrentStep && outputIdCurrentStep && returnAllSelectedTokens)
+            || (skipOutputIdCurrentStep == nullptr && outputIdCurrentStep == nullptr));
+    }
+};
+
+template <typename T>
+[[nodiscard]] size_t getTopPWorkspaceSize(runtime::SizeType32 batchSize, runtime::SizeType32 vocabSizePadded);
+
+[[nodiscard]] std::vector<size_t> getTopPInitWorkspaceSizes(runtime::SizeType32 batchSize);
+
+template <typename T>
+void invokeBatchTopPSampling(TopPSamplingKernelParams<T> const& params, cudaStream_t stream);
+
+void invokeComputeToppDecay(float* runtimeTopP, float const* runtimeInitialTopP, runtime::TokenIdType const** outputIds,
+    float const* topPDecay, float const* topPMin, runtime::TokenIdType const* topPResetIds,
+    runtime::SizeType32 const* sequenceLengths, runtime::SizeType32 const* batchSlots,
+    runtime::SizeType32 localBatchSize, cudaStream_t stream);
+
+template <typename T>
+void invokeBatchAirTopPSampling(TopPSamplingKernelParams<T> const& params, cudaStream_t stream);
+
+template <typename T>
+uint32_t calcAirTopPBlockNum(int batchSize, int len, int smCnt, bool isDeterministic = false);
+
+template <typename T>
+[[nodiscard]] size_t getAirTopPWorkspaceSize(int32_t batchSize, int32_t vocabSizePadded, bool isDeterministic = false);
+
+void invokeSetTopPRuntimeArgs(runtime::SizeType32 batchSize, ScatterDecodingParamEntry<runtime::SizeType32> topK,
+    ScatterDecodingParamEntry<float> topP, bool* skipDecodePtr, float* initialTopPPtr,
+    runtime::SizeType32 const* batchSlotsPtr, bool onDevice, cudaStream_t stream = nullptr);
+
+}
