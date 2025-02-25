a6a05178a688624c448cc5a0a501a95a0b22a192
Dempsey De Clerck <dempsey.de.clerck@hotmail.com>
2025-02-25 16:00:01 +0100

    Add attention mask implementation with CUDA kernels and parameter struct

---
+++ src/attentionMask.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "suggestify/common/cudaUtils.h"
+#include "suggestify/kernels/gptKernels.h"
+#include "suggestify/runtime/iTensor.h"
+#include <cstdint>
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
+#include <sstream>
+
+namespace tc = suggestify::common;
+
+namespace suggestify
+{
+namespace kernels
+{
+
+
+template <typename MaskDataType>
+struct AttentionMaskParams
+{
+    MaskDataType* mask = nullptr;
+    int* cuQSeqLens = nullptr;
+    int const* actualQSeqLens = nullptr;
+    int const* actualKvSeqLens = nullptr;
+    AttentionMaskType attentionMaskType = AttentionMaskType::PADDING;
+    BlockSparseParams blockSparseParams;
+    int batchSize;
+    int maxQSeqLen;
+    int maxKvSeqLen;
+    int slidingWindowSize;
+};
+
+
+template <typename MaskDataType>
+void invokeBuildAttentionMask(AttentionMaskParams<MaskDataType> const& params, cudaStream_t stream);
+
+}
+}
